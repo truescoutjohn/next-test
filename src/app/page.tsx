cd8b2c6b7aeb24b8a2d818ab30e8e01d0ee16c52
Sasha Tkachenko <sashatkachenkobusiness@gmail.com>
2026-02-22 21:17:29 +0200

    feat: add animation and skeletons of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Skeleton } from "@heroui/react";
+import { motion } from "framer-motion";
 import RecipeCard from "../components/common/recipe-card";
 import { useRecipeStore } from "../store/recipe.store";
 import { Button } from "@heroui/react";
@@ -18,12 +20,26 @@ export default function Home() {
 
       {error && <p className="text-red-500 mb-4">{error}</p>}
 
-      {isLoading && <p>Загрузка...</p>}
-
       <div className="max-w-[1024px] w-full mx-auto px-[24px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
+        {isLoading
+          ? Array.from({ length: 6 }).map((_, index) => (
+              <Skeleton key={index} className="rounded-lg">
+                <div className="h-64 w-full" />
+              </Skeleton>
+            ))
+          : recipes.map((recipe, index) => (
+              <motion.div
+                key={recipe.id}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.5,
+                  delay: index * 0.1,
+                }}
+              >
+                <RecipeCard recipe={recipe} />
+              </motion.div>
+            ))}
       </div>
     </>
   );
